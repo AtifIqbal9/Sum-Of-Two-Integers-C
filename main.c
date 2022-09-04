fcfbf7db974fe39ba027fa1ffb21c4d4304e075c
AtifIqbal9 <srkatif1199@gmail.com>
2022-09-04 20:58:53 +0500

    Init Atif

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a ;
+    int b ;
+
+    printf("Enter the first number : ");
+        scanf("%d", &a);
+
+    printf("Enter the Second number : ");
+        scanf("%d", &b);
+
+    
+    
+
+return 0;
+}
